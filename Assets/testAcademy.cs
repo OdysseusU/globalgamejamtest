fd3b75d88832aa41328dc4b8ba806255a8a3fce3
Ulysse Vautier <ulysse.vautier@gmail.com>
2019-12-25 16:30:21 +0100

    Add Barracuda and ML Toolkit

---
+++ Assets/testAcademy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class testAcademy : Academy
+{
+    public override void AcademyReset()
+    {
+        Physics.gravity = new Vector3(0, -resetParameters["gravity"], 0);
+    }
+
+    public override void AcademyStep()
+    {
+    }
+}
