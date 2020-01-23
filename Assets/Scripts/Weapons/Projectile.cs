b71e4a615a606b8ccf499778496c1ca708b33842
Ulysse Vautier <ulysse.vautier@gmail.com>
2020-01-23 22:17:05 +0000

    Added weapons and bullets prefabs and scripts

---
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float velocity = 1f;
+
+    private float time = 0f;
+    private Vector3 fromPosition;
+    private Vector3 toPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fromPosition = transform.position;
+        toPosition = transform.position + transform.up * velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        transform.position = Vector3.Lerp(fromPosition, toPosition, time);
+        if (time > 1f)
+        {
+            time = 0f;
+            fromPosition = transform.position;
+            toPosition = transform.position + transform.up * velocity;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject.Destroy(this.gameObject);
+        //remove other life
+    }
+}
---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float frequency = 1f;
+    public float price = 1f;
+
+    private float time = 0f;
+
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+    }
+
+    public void shoot()
+    {
+        if(time > 1f/frequency)
+        {
+            GameObject inst = Instantiate(projectile, transform.position, transform.rotation);
+            time = 0f;
+        }
+    }
+}
